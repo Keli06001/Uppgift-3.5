ffb361b3e53717120b519fbfb91808f7fef4ece4
Keli06001 <keli06001@utb.vaxjo.se>
2023-10-11 10:56:54 +0200

    Add project files.

---
+++ Uppgift 3.5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+ namespace uppgift3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv två tal");
+            int två=int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            int ett=int.Parse(Console.ReadLine());
+            Console.WriteLine("Välj ett räknesätt\r\n1. Addition\r\n2. Subtraktion\r\n3. Multiplikation\r\n4. Division");
+            string räka=Console.ReadLine();
+            switch (räka)
+            {
+                case "1":
+                    Console.WriteLine(två + "+" + ett + "=");
+                    Console.WriteLine(två + ett);
+                    break;
+                case "2":
+                    Console.WriteLine(två + "-" + ett + "=");
+                    Console.WriteLine(två - ett);
+                    break;
+                case "3":
+                    Console.WriteLine(två + "*" + ett + "=");
+                    Console.WriteLine(två * ett);
+                    break;
+                case "4":
+                    Console.WriteLine(två + "/" + ett + "=");
+                    Console.WriteLine(två / ett);
+                    break;
+            }
+        }
+    }
+}
